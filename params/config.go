35cddb94db9e432e779e9915ffac624ca18b4c94
robertlincecum <rglincecum@gmail.com>
2024-06-21 10:17:08 -0500

    update lighthouse

--- params/config.go
+++ params/config.go
@@ -30,14 +30,14 @@ var (
 	ProgpowGardenGenesisHash     = common.HexToHash("0x8e7ffaeb1893e289ec046bd93a5b5de8b9a50a5fa8e2b30775555c5a6ec18a5d")
 	ProgpowOrchardGenesisHash    = common.HexToHash("0x8e7ffaeb1893e289ec046bd93a5b5de8b9a50a5fa8e2b30775555c5a6ec18a5d")
 	ProgpowLocalGenesisHash      = common.HexToHash("0x32a405efc6e32b60d666a36aee97634f4e2717538d2ae7f13ed7488f299f17db")
-	ProgpowLighthouseGenesisHash = common.HexToHash("0x8e7ffaeb1893e289ec046bd93a5b5de8b9a50a5fa8e2b30775555c5a6ec18a5d")
+	ProgpowLighthouseGenesisHash = common.HexToHash("0x9c388bc3f864ae39bd16dbd97c65e2982b4771ccf2383054f7e6678338c90706")
 
 	// Blake3GenesisHashes
 	Blake3PowColosseumGenesisHash  = common.HexToHash("0x0ca6ff4426eab568de6773fb21d623d3fe6f2f0d2572c0b0b978384373e0895d")
 	Blake3PowGardenGenesisHash     = common.HexToHash("0x02874a9e708fd75a6ed73bf1d6dbc275a87b8914c3f57da0bbdc959c381d0f67")
 	Blake3PowOrchardGenesisHash    = common.HexToHash("0xe9a87eec22a83dc97a33c738bcdade8a524b3254fec0b615632b4e35c3fd986c")
 	Blake3PowLocalGenesisHash      = common.HexToHash("0xecbfee9f6bce9583520d16795bc9850783a8a86563c0ce9a5082d86f497b728f")
-	Blake3PowLighthouseGenesisHash = common.HexToHash("0x43af9e6e91fbf408b9c6fd423ed34e5ad0bd2154737cefc204d145ffc660fc9c")
+	Blake3PowLighthouseGenesisHash = common.HexToHash("0x9c388bc3f864ae39bd16dbd97c65e2982b4771ccf2383054f7e6678338c90706")
 )
 
 // Different Network names
